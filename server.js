96d79858cf69885fec417bcd6a461187e7cf3b76
chromato99 <chromato99@gmail.com>
2021-12-14 22:41:52 +0900

    update game page

--- server.js
+++ server.js
@@ -179,7 +179,8 @@ app.post('/newroom', (req, res) => {
         let room = {
             name: req.body.roomname,
             players: new Array(),
-            max_player_count: req.body.max
+            max_player_count: req.body.max,
+            ready: 0
         };
         roomList.push(room);
         
@@ -191,15 +192,24 @@ app.get('/game/:roomname', (req, res) => {
     if(!req.user) {
         res.redirect('/login');
     } else {
+        let room;
         roomList.forEach((elem) => {
+            if(req.params.roomname == elem.name) {
+                room = elem;
+            }
             if(elem.players.length == elem.max_player_count) {
                 res.redirect('/lobby');
             }
         });
-        res.render('RPSgame', {username: req.user.username, roomname: req.params.roomname});
+        if(!room) {
+            console.log("no such room : ", roomname, "| username : ",req.user.username);
+            res.redirect('/lobby');
+        } else {
+            res.render('RPSgame', {username: req.user.username, roomname: req.params.roomname, room: room});
+        }
+        
     }
 });
-
 
 
 lobbyIO.use((socket, next) => {
@@ -268,24 +278,122 @@ lobbyIO.on('connection', (socket) => {
         });
     });
 
+    socket.on('disconnect', (reason) => {
+    });
 });
 
+function updateDraw(userList) {
+    userList.forEach((elem) => {
+        db.query('UPDATE user SET draw=draw+1 WHERE username=?', [elem.name]);
+    });
+}
+
+function updateWinLoss(userList, winSelection) {
+    userList.forEach((elem) => {
+        if(elem.selection == winSelection) {
+            db.query('UPDATE user SET win=win+1 WHERE username=?', [elem.name]);
+        } else {
+            db.query('UPDATE user SET loss=loss+1 WHERE username=?', [elem.name]);
+        }
+    });
+}
 
 gameIO.on('connection', (socket) => {   //연결이 들어오면 실행되는 이벤트
     // socket 변수에는 실행 시점에 연결한 상대와 연결된 소켓의 객체가 들어있다.
     socket.on('newPlayer', (roomname) => {
         socket.data.roomname = roomname;
         socket.join(roomname);
+        
+        
         roomList.forEach((elem) => {
             if(elem.name == roomname) {
-                if(!elem.players.includes(socket.request.user.username)) {
-                    elem.players.push(socket.request.user.username);
-                }
+                elem.players.push({
+                    name: socket.request.user.username,
+                    ready: false,
+                    status: '',
+                    selection: 0,
+                });
+                socket.data.room = elem;
             }
         });
-        gameIO.to(socket.data.roomname).emit('message', socket.request.user.username + ' joined!!')
+        gameIO.to(socket.data.roomname).emit('resRoomData', socket.data.room);
+        gameIO.to(socket.data.roomname).emit('message', socket.request.user.username + ' joined!!');
+    });
+
+    socket.on('reqRoomData', () => {
+        socket.emit('resRoomData', socket.data.room);
+    });
+
+    socket.on('ready', () => {
+        socket.data.room.players.forEach((elem) => {
+            if(elem.name == socket.request.user.username) {
+                elem.ready = true;
+                elem.status = 'ready';
+            }
+        });
+        socket.data.room.ready++;
+        socket.emit('resRoomData', socket.data.room);
+        gameIO.to(socket.data.roomname).emit('message', socket.request.user.username + ' Ready');
+        if(socket.data.room.players.length == socket.data.room.ready) {
+            socket.data.room.ready = 0;
+            socket.emit('gameStarted');
+        }
     });
     
+    socket.on('setSelection', (selectionName, selection) => {
+        socket.data.room.players.forEach((elem) => {
+            if(elem.name == socket.request.user.username) {
+                elem.status = selectionName;
+                elem.selection = selection;
+            }
+        });
+        socket.data.room.ready++;
+        gameIO.to(socket.data.roomname).emit('message', socket.request.user.username + ' Selected');
+        if(socket.data.room.players.length == socket.data.room.ready) {
+            socket.data.room.ready = 0;
+
+            // Find winner
+            let rock = 0;
+            let paper = 0;
+            let scissor = 0;
+
+            socket.data.room.players.forEach((elem) => {
+                if(elem.selection == 1) {
+                    rock++;
+                } else if(elem.selection == 2) {
+                    paper++;
+                } else if(elem.selection == 3) {
+                    scissor++;
+                } else {
+                    console.log('Something wrong when collect selection');
+                }
+            });
+
+            if(rock > 0 && paper > 0 && scissor > 0) {
+                updateDraw(socket.data.room.players);
+                gameIO.to(socket.data.roomname).emit('resRoomData', socket.data.room);
+                gameIO.to(socket.data.roomname).emit('draw');
+            } else if((rock == 0 && scissor == 0) || (rock == 0 && paper == 0) || (scissor == 0 && paper == 0)) {
+                updateDraw(socket.data.room.players);
+                gameIO.to(socket.data.roomname).emit('resRoomData', socket.data.room);
+                gameIO.to(socket.data.roomname).emit('draw');
+            } else if(rock == 0) {
+                updateWinLoss(socket.data.room.players, 3);
+                gameIO.to(socket.data.roomname).emit('resRoomData', socket.data.room);
+                gameIO.to(socket.data.roomname).emit('winloss', socket.data.room.players,3);
+            } else if(paper == 0) {
+                updateWinLoss(socket.data.room.players, 1);
+                gameIO.to(socket.data.roomname).emit('resRoomData', socket.data.room);
+                gameIO.to(socket.data.roomname).emit('winloss', socket.data.room.players, 1);
+            } else if(scissor == 0) {
+                updateWinLoss(socket.data.room.players, 2);
+                gameIO.to(socket.data.roomname).emit('resRoomData', socket.data.room);
+                gameIO.to(socket.data.roomname).emit('winloss', socket.data.room.players, 2);
+            } else {
+                console.log('Something wrong when find winner');
+            }
+        }
+    });
 
     socket.on('sendChat', (msg, username) => {
         gameIO.to(socket.data.roomname).emit('emitChat', msg, username);
@@ -304,9 +412,29 @@ gameIO.on('connection', (socket) => {   //연결이 들어오면 실행되는 �
                     id: elem.id,
                     username: elem.request.user.username
                 });
-            }); 
+            });
         }
         socket.emit('resLobbyUserList', userList);
+    });
+
+    socket.on('disconnect', (reason) => {
+        console.log(reason, socket.request.user.username);
+        for(let i = 0; i < socket.data.room.players.length; i++) {
+            if(socket.data.room.players[i].name == socket.request.user.username) {
+                socket.data.room.players.splice(i, 1);
+                break;
+            }
+        }
+        if(socket.data.room.players.length == 0) {
+            for(let i = 0; i < roomList.length; i++) {
+                if(roomList[i].name == socket.data.room.name) {
+                    roomList.splice(i, 1);
+                    return;
+                }
+            }
+        }
+        gameIO.to(socket.data.roomname).emit('resRoomData', socket.data.room);
+        gameIO.to(socket.data.roomname).emit('userDisconnected', socket.request.user.username);
     });
 });
 
